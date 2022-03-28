80ffeea601158250e51c4e1ba680bef0c4e5e28b
Yothin Inbanleng <yothinin@hotmail.com>
2022-03-29 00:54:00 +0700

    Use data struct to store information.

--- ex_from_glade.c
+++ ex_from_glade.c
@@ -6,6 +6,43 @@
 
 #include <gtk/gtk.h>
 
+typedef struct
+{
+  const gchar    *time;
+  const gchar    *dest;
+  const gchar    *busno;
+  const gchar    *standard;
+  const gchar    *platform;
+  const gchar    *note;
+  const gboolean out;
+  const gchar    *objname;
+}
+Table;
+
+enum
+{
+  COL_TIME,
+  COL_DEST,
+  COL_BUSNO,
+  COL_STANDARD,
+  COL_PLATFORM,
+  COL_NOTE,
+  NUM_COLUMNS
+};
+
+static Table data[] =
+{
+  {"17:00", "เชียงใหม่", "18-102", "ม.1ข", "1", "", FALSE},
+  {"17:30", "เชียงของ", "962-1", "ม.4ข", "3", "", FALSE},
+  {"18:00", "แม่สาย", "957-3", "ม.1ก", "ประตู 3", "", FALSE},
+  {"18:05", "เชียงราย", "909-12", "ม.4ข", "4", "", FALSE},
+  {"18:30", "แม่ฮ่องสอน", "961-1", "ม.1พ", "5", "", FALSE},
+  {"19:00", "เชียงใหม่", "18-1", "ม.1ข", "1", "", FALSE},
+  {"19:15", "เชียงใหม่", "18-1", "ม.1ก", "ประตู 3", "รถมาจากหมอชิต, รถติดหน้าหมอชิต", FALSE},
+  {"19:30", "เชียงราย", "909-1", "ม.1ก", "ประตู 3", "", FALSE},
+  {"19:45", "น่าน", "910-2", "ม.1ข", "4", "", FALSE},
+};
+
 G_MODULE_EXPORT
 void treeviewEvent(GtkWidget *treeview, GdkEventButton *event, gpointer userdata)
 {
@@ -14,8 +51,6 @@ void treeviewEvent(GtkWidget *treeview, GdkEventButton *event, gpointer userdata
 
   if (event->type == GDK_BUTTON_PRESS && event->button == 1)
     g_print("Selected...\n");
-
-
 
 }
 
@@ -55,17 +90,15 @@ GdkPixbuf
 int main(int argc, char** argv)
 {
   
-  //GdkPixbuf *icon;
-  //icon = create_pixbuf("Digital-Signage.png");
+  GdkPixbuf *icon;
+  icon = create_pixbuf("Digital-Signage.png");
   
 	gtk_init(&argc, &argv);
 
-    // Create a builder object that will load the file.
-    GtkBuilder* builder = gtk_builder_new();
-
-    // Load the XML from a file.
-    gtk_builder_add_from_file(builder, "ex_tree.glade", NULL);
-    
+  GtkBuilder* builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "ex_from_glade.xml", NULL);
+  gtk_builder_connect_signals(builder, NULL);
+  
     GSList *lst, *objList = gtk_builder_get_objects(builder);
 
     g_print("%p\n", objList);
@@ -73,29 +106,24 @@ int main(int argc, char** argv)
       g_print("%p\n", (char*)(lst->data));
     }
     
+  GObject* window = gtk_builder_get_object(builder, "window");
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_icon(GTK_WINDOW(window), icon);
+ 
+  GtkListStore *store = GTK_LIST_STORE(gtk_builder_get_object(builder, "liststore1"));
+  GtkTreeIter iter;
 
-    //GtkWidget *butt1 = GTK_WIDGET(gtk_builder_get_object(builder, "hbuttonbox1"));
-    ///GtkWidget *butt2 = GTK_WIDGET(gtk_builder_get_object(builder, "ok_button"));
-    
-    ///g_signal_connect(butt2, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  int i;
+  for (i = 0; i < G_N_ELEMENTS (data); i++){
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(
+      store, &iter, COL_TIME, data[i].time, COL_DEST, data[i].dest, COL_BUSNO, data[i].busno, COL_STANDARD, data[i].standard, 
+      COL_PLATFORM, data[i].platform, COL_NOTE, data[i].note, -1);
+  }   
 
-    gtk_builder_connect_signals(builder, NULL);
-    
-    //g_signal_connect(butt1, "clicked", G_CALLBACK(btnClicked), NULL);
-    // Quit the app when the window is closed.
-    //g_signal_connect(GTK_WINDOW(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_widget_show_all(GTK_WIDGET(window));
 
-    // Get the object called 'main_window' from the file and show it.
-    GObject* window = gtk_builder_get_object(builder, "window");
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WINDOW_TOPLEVEL);
-    //gtk_window_set_icon(GTK_WINDOW(window), icon);
-    //gtk_window_fullscreen(GTK_WINDOW(window));
+  gtk_main();
 
-
-    gtk_widget_show_all(GTK_WIDGET(window));
-
-    // Main loop.
-    gtk_main();
-
-    return 0;
+  return 0;
 }
